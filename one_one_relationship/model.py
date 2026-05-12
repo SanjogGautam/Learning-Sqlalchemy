5224074e9fa2c2707d0ed0f4740153803d51bc35
SanjogGautam <164294030+SanjogGautam@users.noreply.github.com>
2026-05-12 10:39:02 +0545

    sanjog

---
+++ one_one_relationship/model.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey,UniqueConstraint
+from sqlalchemy.orm import declarative_base,relationship,sessionmaker
+db_url="sqlite:///mydb.db"
+engine=create_engine(db_url)
+Session=sessionmaker(bind=engine)
+session=Session()
+Base=declarative_base()
+class Basemodel(Base):
+    __abstract__=True
+    __allow_unmapped__=True
+    id=Column(Integer,primary_key=True)
+class Addresses(Basemodel):
+    __tablename__="addresses"
+    city=Column(String)
+    state=Column(String)
+    zip_code=Column(Integer)
+    user_id=Column(ForeignKey("users.id"))
+    user=relationship("User",back_populates="addresses")
+    def __repr__(self):
+        return f"Addresses(city={self.city}, state={self.state}, zip_code={self.zip_code})"
+class User(Basemodel):
+    __tablename__="users"
+    name=Column(String)
+    email=Column(String)
+    addresses=relationship("Addresses")
+    def __repr__(self):
+        return f"User(name={self.name}, email={self.email})"
+Base.metadata.create_all(engine)
---
+++ one_one_relationship/non_mapped.py
@@ -0,0 +1,20 @@
+from model import session,Addresses,User
+#creating users
+user1=User(name="Sarin Pradhan",email="sarinpradhan@gmail.com")
+user2=User(name="Rajesh Kumar",email="Rajeskumar@gmail.com")
+#creating addresses
+address1=Addresses(city="Kathmandu",state="Bagmati",zip_code=1232)
+address2=Addresses(city="Bhaktapur",state="Bagmati",zip_code=1234)
+address3=Addresses(city="Lalitpur",state="Bagmati",zip_code=1235)
+#associating addresses with users
+user1.addresses.extend([address1,address2])
+user2.addresses.append(address3)
+#adding users to the session and commmiting the changes
+session.add(user1)
+session.add(user2)
+session.commit()
+
+print(f"{user1.addresses= }")
+print(f"{user2.addresses= }")
+#backward maping:
+print(f"{address1.user}")
